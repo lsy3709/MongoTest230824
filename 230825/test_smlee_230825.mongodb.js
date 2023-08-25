f992b81d884b27373e3b0b61813065503ddf6c29
miguellee91 <42338783+miguellee91@users.noreply.github.com>
2023-08-25 14:56:12 +0900

    Update test_smlee_230825.mongodb.js

--- 230825/test_smlee_230825.mongodb.js
+++ 230825/test_smlee_230825.mongodb.js
@@ -5,3 +5,24 @@ db.by_type.find({ type: '차대차', accident_count: { $gte: 100 } });
 
 // 전국의  ‘차대사람’ 사고  중에서  사망자수가  0회이거나  중상자수가  0회인  지역을  출력
 db.by_type.find({ type: '차대사람', $or: [{ casualties: 0 }, { heavy_injury: 0 }] });
+
+//  ‘차대사람’ 사고  중에서  사망자수가  0회이거나  중상자수가  100회인  지역을  출력
+db.by_type.find({ type: '차대사람', $or: [{ casualties: 0 }, { heavy_injury: 100 }] });
+
+// 검색 결과
+// {
+//   _id: ObjectId("5c89102b0da47a850775de31"),
+//   area_id: ObjectId("5c88f9f70da47a8507752858"),
+//   city_or_province: '대전',
+//   county: '대덕구',
+//   type: '차대사람',
+//   accident_count: 244,
+//   casualties: 236,
+//   death_toll: 16,
+//   heavy_injury: 100,
+//   injury_report: 16,
+//   light_injury: 120
+// }
+
+//mnthdata 자료만 출력하기
+db.by_month.find({}, { month_data: 1 });
